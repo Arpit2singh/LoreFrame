41c7d19f6e46c7f5a7f7a8a4d3e071af707cb630
Bedanta Mahanty <mahantybedanta@gmail.com>
2026-01-31 20:29:00 +0530

    DashBoard integrate

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Auth from './pages/Auth';
+import DashFront from './pages/DashFront';
 const App = () => {
   return (
     <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/Tool" element={<HomePage />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={<DashFront/>}/>
       </Routes>
   )
 }
